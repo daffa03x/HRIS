9858059378448f0c6a1fff741daff07299378ba8
daffa03x <mobadaff@gmail.com>
2024-07-23 16:03:55 +0700

    update & delete employee

--- app/employee/router.js
+++ app/employee/router.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var router = express.Router();
-const { index, create, store } = require("./controller");
+const { index, create, store, edit, update, destroy } = require("./controller");
 
 router.get("/", index);
 router.get("/create", create);
 router.post("/store", store);
+router.get("/edit/:id", edit);
+router.put("/update/:id", update);
+router.delete("/destroy/:id", destroy);
 
 module.exports = router;
