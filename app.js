da305d987f9ffb637e6d4ca1c5823ffa40f04655
daffa03x <mobadaff@gmail.com>
2024-07-31 16:12:06 +0700

    alert

--- app.js
+++ app.js
@@ -4,16 +4,29 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var methodOverride = require("method-override");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 // Variable Route
 const dashboardRouter = require("./app/dashboard/router");
 const employeeRouter = require("./app/employee/router");
+const recruitmentRouter = require("./app/recruitment/router");
 
 var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {},
+  })
+);
+app.use(flash());
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -27,6 +40,7 @@ app.use(methodOverride("_method"));
 // Use Route
 app.use("/dashboard", dashboardRouter);
 app.use("/employee", employeeRouter);
+app.use("/recruitment", recruitmentRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- app/employee/controller.js
+++ app/employee/controller.js
@@ -1,23 +1,32 @@
 const Employee = require("./model");
-const db = require("../../db/mysql2");
 
 module.exports = {
   index: async (req, res) => {
     try {
+      const alertMessage = req.flash("alertMessage");
+      const alertStatus = req.flash("alertStatus");
+
+      const alert = { message: alertMessage, status: alertStatus };
       const employees = await Employee.findAll();
-      res.render("employee/index", { employees });
+      res.render("employee/index", { employees, alert });
     } catch (error) {
       console.error("Error fetching employees:", error);
       res.status(500).send("Internal Server Error");
     }
   },
   create: (req, res) => {
-    res.render("employee/create");
+    const alertMessage = req.flash("alertMessage");
+    const alertStatus = req.flash("alertStatus");
+
+    const alert = { message: alertMessage, status: alertStatus };
+    res.render("employee/create", { alert });
   },
   store: async (req, res) => {
     const { first_name, last_name, address, phone, email, position, department, date_of_joining, date_of_birth } = req.body;
 
     if (!first_name || !last_name || !address || !phone || !email || !position || !department || !date_of_joining || !date_of_birth) {
+      req.flash("alertMessage", "Lengkapi semua kolom");
+      req.flash("alertStatus", "danger");
       return res.redirect("/employee/create");
     }
 
@@ -33,6 +42,8 @@ module.exports = {
         date_of_joining,
         date_of_birth,
       });
+      req.flash("alertMessage", "Berhasil tambah pegawai");
+      req.flash("alertStatus", "success");
 
       res.redirect("/employee");
     } catch (error) {
@@ -44,7 +55,11 @@ module.exports = {
     const { id } = req.params;
     try {
       const employee = await Employee.findByPk(id);
-      res.render("employee/edit", { employee });
+      const alertMessage = req.flash("alertMessage");
+      const alertStatus = req.flash("alertStatus");
+
+      const alert = { message: alertMessage, status: alertStatus };
+      res.render("employee/edit", { employee, alert });
     } catch (error) {
       console.error("Error fetching employee:", error);
       res.status(500).send("Internal Server Error");
@@ -68,6 +83,9 @@ module.exports = {
         },
         { where: { employee_id: id } }
       );
+      req.flash("alertMessage", "Berhasil ubah pegawai");
+      req.flash("alertStatus", "success");
+
       res.redirect("/employee");
     } catch (error) {
       console.error("Error updating employee:", error);
@@ -78,6 +96,9 @@ module.exports = {
     const { id } = req.params;
     try {
       await Employee.destroy({ where: { employee_id: id } });
+      req.flash("alertMessage", "Berhasil hapus pegawai");
+      req.flash("alertStatus", "success");
+
       res.redirect("/employee");
     } catch (error) {
       console.error("Error deleting employee:", error);
