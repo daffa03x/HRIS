4b9771d14f36962aa970ace95b999f00bcdd95d9
daffa03x <mobadaff@gmail.com>
2024-11-22 09:30:52 +0700

    fixing db

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("hris", "root", "", {
-  host: "localhost",
+const sequelize = new Sequelize("bpvd3qwphxrkmsqmynfk", "ugmhnsddjdsz5ins", "91lMQvHqYFEniedHvSU6", {
+  host: "bpvd3qwphxrkmsqmynfk-mysql.services.clever-cloud.com",
   dialect: "mysql",
 });
 // Sync database
